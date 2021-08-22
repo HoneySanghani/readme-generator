be40f8fcdbe893de68079f79584a19bb96c2e4eb
Honey Sanghani <honeysanghani98@gmail.com>
2021-08-22 12:15:25 -0400

    basic main file created

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# readme-generator
+
+  # sfdsgedd
+  ## Description
+  sgdhtegfdhfjgjfg
+  ## License
+  MIT
+  ## Dependencies
+  sdfgdfge
+  ## How to run test
+  sfefe
+  ## For further Questions send me an E-mail
+  dfgfhfh
+
---
+++ app.js
@@ -0,0 +1,80 @@
+// TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer=require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+
+// TODO: Create an array of questions for user input
+const questions = ()=>{
+    return inquirer.prompt([
+        {
+            type:'input',
+            name:'username',
+            message:'What is your GitHUb user name?',
+        },
+        {
+            type:'input',
+            name:'email',
+            message:'What is your email address?'
+        },
+        {
+            type:'input',
+            name:'title',
+            message:'What is your project name?'
+        },
+        {
+            type:'input',
+            name:'description',
+            message:'Please write a short description of your project.'
+        },
+        {
+            type:'list',
+            name:'lisence',
+            message:'What kind of license should your project have?',
+            choices:['MIT','APACHE 2.0','GPL 3.0','BSD 3','None'],
+        },
+        {
+            type:'input',
+            name:'dependencies',
+            message:'What command should be run to install dependencies?'
+        },
+        {
+            type:'input',
+            name:'test',
+            message:'What command should be run to run test?',
+        }
+    ])
+};
+
+// TODO: Create a function to write README file
+const writeToFile=(data)=> {
+    return new Promise((resolve,reject)=>{
+        fs.writeFile('Readme.md',data,err=>{
+            if(err){
+                reject(err);
+                return;
+            }
+            resolve({
+                ok:true,
+                message:'File created!'
+            })
+        })
+    })
+}
+
+// TODO: Create a function to initialize app
+const init=()=> {
+    questions()
+    .then((answers)=>{
+        return generateMarkdown(answers);
+    })
+    .then((content)=>{
+        return writeToFile(content);
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
+}
+
+// Function call to initialize app
+init();
+
